1009ae22ee78d543668a2fe6cd956080d41d0751
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-13 10:56:30 +0200

    feat: Added blog post simple template

---
+++ src/templates/blog-post.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+function BlogPost(props) {
+	const post = props.data.markdownRemark;
+	const { title, date } = post.frontmatter;
+	console.log(post);
+	return (
+		<Layout>
+			<div>
+				<h1>{title}</h1>
+				<div dangerouslySetInnerHTML={{ __html: post.html }} />
+			</div>
+		</Layout>
+	)
+}
+
+export default BlogPost;
+
+export const query = graphql`
+	query PostQuery($slug: String!) {
+		markdownRemark(fields: { slug: { eq: $slug } }) {
+				html
+				excerpt
+				frontmatter {
+						title
+						image
+				}
+		}
+	}
+`;
