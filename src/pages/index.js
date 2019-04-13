02d4c0baef61f02cc3fc34b6bbfd324df4367897
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-13 10:11:05 +0200

    feat: Display posts on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,42 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = (props) => {
+	const postList = props.data.allMarkdownRemark;
+	return (
+		<Layout>
+			{postList.edges.map(({ node }, i) => (
+				<Link to={node.fields.slug} key={i} className="link" >
+					<div className="link" >
+						<h1>{node.frontmatter.title}</h1>
+						<span>{node.frontmatter.date}</span>
+						<p>{node.excerpt}</p>
+					</div>
+				</Link>
+			))}
+		</Layout>
+	)
+}
 
-export default IndexPage
+export default IndexPage;
+
+export const listQuery = graphql`
+	query ListQuery {
+		allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }){
+			edges{
+				node{
+					frontmatter{
+						title
+						date(formatString: "MMMM Do YYYY")
+						tags
+					}
+					excerpt(pruneLength: 250)
+					fields{
+						slug
+					}
+				}
+			}
+		}
+	}
+`
