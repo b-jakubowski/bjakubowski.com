4bf2d6710446fe4aa41f432bf413022f0298e6d5
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-13 18:48:51 +0200

    feat: Added previous next post component

---
+++ src/components/previous-next-post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PreviousNextPost = (props) => {
+
+	const { prev, next } = props
+
+	return (
+		<ul>
+			{prev && <li><a href={prev.fields.slug}>
+				Previous {" "}
+				{prev.frontmatter.title}</a></li>}
+			{next && <li><a href={next.fields.slug}>
+				Next  {" "} {next.frontmatter.title}
+
+			</a></li>}
+		</ul>
+	)
+}
+
+export default PreviousNextPost;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import Layout from '../components/layout';
+import { graphql } from 'gatsby'
+import PreviousNextPost from '../components/previous-next-post';
 
 function BlogPost(props) {
 	const post = props.data.markdownRemark;
 	const { title, date } = post.frontmatter;
+	const { prev, next } = props.pageContext;
 
 	return (
 		<Layout>
@@ -13,6 +16,7 @@ function BlogPost(props) {
 				<h1>{title}</h1>
 				<h3>{date}</h3>
 				<div dangerouslySetInnerHTML={{ __html: post.html }} />
+				<PreviousNextPost prev={prev && prev.node} next={next && next.node} />
 			</div>
 		</Layout>
 	)
@@ -22,22 +26,22 @@ export default BlogPost;
 
 export const query = graphql`
 	query PostQuery($slug: String!) {
-		markdownRemark(fields: { slug: { eq: $slug } }) {
+			markdownRemark(fields: { slug: { eq: $slug } }) {
 				html
 				excerpt
 				frontmatter {
-						title
-						image {
-							childImageSharp {
-								resize(width: 1500, height: 1500) {
-									src
-								}
-								fluid(maxWidth: 786) {
-									...GatsbyImageSharpFluid
-								}
+					title
+					image {
+						childImageSharp {
+							resize(width: 1000, height: 420) {
+								src
+							}
+							fluid(maxWidth: 786) {
+								...GatsbyImageSharpFluid
 							}
 						}
+					}
 				}
 		}
 	}
-`;
+`
