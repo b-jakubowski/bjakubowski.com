7a25b27480064ce90681124d1ed9627a920cfe1c
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-24 16:41:32 +0200

    fix: Updated header according to custom material theme colors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,26 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React from "react";
-import "./header.css";
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 import Button from "@material-ui/core/Button";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import './header.scss';
+import theme from '../theme';
+
 
 const Header = ({ siteTitle }) => (
-	<AppBar position="static" color="default">
-		<Toolbar className="header-elements">
-			<Typography variant="h6" color="inherit">{siteTitle}</Typography>
-			<Button variant="contained" color="default">
-				<Link to="/tags" className="header-btn-txt">Tags</Link>
-			</Button>
-		</Toolbar>
-	</AppBar>
+	<MuiThemeProvider theme={theme}>
+		<AppBar position="static" color="primary" className="header">
+			<Toolbar className="header-elements">
+				<Typography variant="h6" color="secondary">{siteTitle}</Typography>
+				<Button variant="contained" color="secondary">
+					<Link to="/tags" className="header-btn-txt">Tags</Link>
+				</Button>
+			</Toolbar>
+		</AppBar>
+	</MuiThemeProvider>
 )
 
 Header.propTypes = {
