7cb8a816f2d693b0113093a8642c9318ce1e13a1
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-27 17:21:46 +0200

    feat: Added margin top to content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,13 +18,13 @@ const Layout = ({ children }) => (
     `}
 		render={data => (
 			<>
-				<Header siteTitle={data.site.siteMetadata.title} />
+				<Header />
 				<div
 					style={{
 						margin: `0 auto`,
 						maxWidth: 960,
 						padding: `0px 1.0875rem 1.45rem`,
-						paddingTop: 0,
+						marginTop: '4em',
 					}}
 				>
 					<main>{children}</main>
