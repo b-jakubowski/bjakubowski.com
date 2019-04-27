841c16a76709d964f91f0d81680a303b8ed4af7c
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-27 17:22:15 +0200

    fix: Added theme provider to 404, index, tags pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,26 @@ import { Link, graphql } from "gatsby";
 import Img from 'gatsby-image';
 
 import Layout from '../components/layout';
+import { MuiThemeProvider } from "@material-ui/core";
+import theme from '../theme';
 
 const IndexPage = (props: { data: { allMarkdownRemark: any; }; }) => {
 	const postList = props.data.allMarkdownRemark;
 	return (
-		<Layout>
-			{postList.edges.map(({ node }, i: number) => (
-				<Link to={node.fields.slug} key={i} className="link" >
-					<div className="link" >
-						<Img fluid={node.frontmatter.image.childImageSharp.fluid} />
-						<h1>{node.frontmatter.title}</h1>
-						<span>{node.frontmatter.date}</span>
-						<p>{node.excerpt}</p>
-					</div>
-				</Link>
-			))}
-		</Layout>
+		<MuiThemeProvider theme={theme}>
+			<Layout>
+				{postList.edges.map(({ node }, i: number) => (
+					<Link to={node.fields.slug} key={i} className="link" >
+						<div className="link" >
+							<Img fluid={node.frontmatter.image.childImageSharp.fluid} />
+							<h1>{node.frontmatter.title}</h1>
+							<span>{node.frontmatter.date}</span>
+							<p>{node.excerpt}</p>
+						</div>
+					</Link>
+				))}
+			</Layout>
+		</MuiThemeProvider>
 	)
 }
 
--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link, graphql } from "gatsby"
 import Layout from '../components/layout';
+import { MuiThemeProvider } from "@material-ui/core";
+import theme from '../theme';
 
 function TagsPage(props) {
 	const data = props.data.allMarkdownRemark.group
 
 	return (
-		<Layout>
-			<div className="tags">
-				<h1>All tags</h1>
-				{
-					data.map(tag => (
-						<Link to={`/${tag.fieldValue}`} >
-							{tag.fieldValue} {`(${tag.totalCount})`}
-						</Link>
-					))
-				}
-			</div>
-		</Layout>
+		<MuiThemeProvider theme={theme}>
+			<Layout>
+				<div className="tags">
+					<h1>All tags</h1>
+					{
+						data.map(tag => (
+							<Link to={`/${tag.fieldValue}`} >
+								{tag.fieldValue} {`(${tag.totalCount})`}
+							</Link>
+						))
+					}
+				</div>
+			</Layout>
+		</MuiThemeProvider>
 	)
 }
 
