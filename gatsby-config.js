a01a82063ffacac5d569a8f025489d063b52da31
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-27 17:12:00 +0200

    feat: Added gatsby google fonts plugin with material icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,14 @@ module.exports = {
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `images`,
-				path: `${__dirname}/src/images`,
+				path: `${__dirname}/src/images`
+			},
+			resolve: 'gatsby-plugin-google-fonts',
+			options: {
+				fonts: [
+					'material icons',
+					'roboto:300,400,500,700',
+				],
 			},
 		},
 		`gatsby-transformer-sharp`,
@@ -28,7 +35,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+				icon: `src/images/logo-icon.png`,
 			},
 		},
 		`gatsby-transformer-remark`,
