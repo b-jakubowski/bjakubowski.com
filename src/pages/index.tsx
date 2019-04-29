fdc687a23cff4a756b902b0cc02e3e34cff55df2
Bartosz Jakubowski <bjakubov@gmail.com>
2019-04-29 20:46:34 +0200

    refactor: Moved main page blog post card to new component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
-import Img from 'gatsby-image';
+import { graphql } from "gatsby";
 
 import Layout from '../components/layout';
 import { MuiThemeProvider } from "@material-ui/core";
 import theme from '../theme';
+import BlogPostsList from "../components/blog-posts-list";
 
 const IndexPage = (props: { data: { allMarkdownRemark: any; }; }) => {
 	const postList = props.data.allMarkdownRemark;
+
 	return (
 		<MuiThemeProvider theme={theme}>
 			<Layout>
 				{postList.edges.map(({ node }, i: number) => (
-					<Link to={node.fields.slug} key={i} className="link" >
-						<div className="link" >
-							<Img fluid={node.frontmatter.image.childImageSharp.fluid} />
-							<h1>{node.frontmatter.title}</h1>
-							<span>{node.frontmatter.date}</span>
-							<p>{node.excerpt}</p>
-						</div>
-					</Link>
+					<BlogPostsList node={node} key={i} />
 				))}
 			</Layout>
 		</MuiThemeProvider>
@@ -27,6 +21,7 @@ const IndexPage = (props: { data: { allMarkdownRemark: any; }; }) => {
 }
 
 export default IndexPage;
+
 
 export const listQuery = graphql`
 	query ListQuery {
