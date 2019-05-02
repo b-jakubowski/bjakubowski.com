78f2ead67b7d9a274ca0242459663778bd7ce43c
Bartosz Jakubowski <bjakubov@gmail.com>
2019-05-02 18:54:34 +0200

    feat: Added styles to previous-next-post component

--- src/components/previous-next-post.tsx
+++ src/components/previous-next-post.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
+import { Button, Tooltip } from '@material-ui/core';
+
+const buttonText = {
+	textDecoration: 'none',
+	color: 'white'
+}
+
+const buttonContainer = {
+	display: 'flex',
+	width: '100%'
+}
 
 const PreviousNextPost = (props) => {
-
 	const { prev, next } = props
 
 	return (
-		<ul>
-			{prev && <li><a href={prev.fields.slug}>
-				Previous {" "}
-				{prev.frontmatter.title}</a></li>}
-			{next && <li><a href={next.fields.slug}>
-				Next  {" "} {next.frontmatter.title}
-
-			</a></li>}
-		</ul>
+		<div style={{ display: 'flex' }}>
+			{prev &&
+				<div style={buttonContainer}>
+					<Tooltip title={prev.frontmatter.title}>
+						<Button color="secondary" variant="contained">
+							<span className="material-icons">keyboard_arrow_left</span>
+							<a href={prev.fields.slug} style={buttonText}>Prev post</a>
+						</Button>
+					</Tooltip>
+				</div>
+			}
+			{next &&
+				<div style={buttonContainer}>
+					<div style={{ flexGrow: 1 }}></div>
+					<Tooltip title={next.frontmatter.title}>
+						<Button color="secondary" variant="contained">
+							<a href={next.fields.slug} style={buttonText}>Next post</a>
+							<span className="material-icons">keyboard_arrow_right</span>
+						</Button>
+					</Tooltip>
+				</div>
+			}
+		</div>
 	)
 }
 
