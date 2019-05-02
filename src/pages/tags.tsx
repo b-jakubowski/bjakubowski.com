9a7d370c3c96b5f77fc41500c0bcb104764a203f
Bartosz Jakubowski <bjakubov@gmail.com>
2019-05-02 18:52:49 +0200

    feat: Added tags props interface and types

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link, graphql } from "gatsby"
-import Layout from '../components/layout';
 import { MuiThemeProvider } from "@material-ui/core";
+import Layout from '../components/layout';
 import theme from '../theme';
 
-function TagsPage(props) {
-	const data = props.data.allMarkdownRemark.group
+export interface Tag {
+	[x: string]: any;
+	fieldValue: React.ReactNode;
+	totalCount: number;
+}
+
+export interface tagsProps {
+	data: {
+		allMarkdownRemark: {
+			group: Tag;
+		};
+	};
+}
+
+function TagsPage(props: tagsProps) {
+	const data = props.data.allMarkdownRemark.group;
 
 	return (
 		<MuiThemeProvider theme={theme}>
@@ -13,7 +27,7 @@ function TagsPage(props) {
 				<div className="tags">
 					<h1>All tags</h1>
 					{
-						data.map((tag, index) => (
+						data.map((tag: Tag, index: number) => (
 							<Link to={`/${tag.fieldValue}`} key={index}>
 								{tag.fieldValue} {`(${tag.totalCount})`}
 							</Link>
