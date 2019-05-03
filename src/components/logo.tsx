75154e9177bf2565dbf6e72167de03bb0eedc13a
Bartosz Jakubowski <bjakubov@gmail.com>
2019-05-03 11:15:18 +0200

    fix: Refactored and changed logo component styles

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,12 +2,24 @@ import { Link } from 'gatsby';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import logoImg from '../images/logo-icon.png';
-import './logo.scss';
+import theme from '../theme';
+
+const logo = {
+	display: 'flex',
+	alignItems: 'center',
+	textDecoration: 'none',
+	flexDirection: 'column'
+}
+
+const logoImage = {
+	width: '50px',
+	marginBottom: '0.5em'
+}
 
 const Logo = () => (
-	<Link to="/" className="logo">
-		<img src={logoImg} alt="bj-logo" className='logo-img' />
-		<Typography variant="h6" color="secondary" className="logo-title">bartek jakubowski</Typography>
+	<Link to="/" style={logo}>
+		<img src={logoImg} alt="bj-logo" style={logoImage} />
+		<Typography variant="h6" style={{ color: theme.palette.secondary.dark }}>Bartek Jakubowski</Typography>
 	</Link>
 )
 
