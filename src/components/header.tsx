d886ffe3282fa8862e9905cc4c9a8df661cdd057
Bartosz Jakubowski <bjakubov@gmail.com>
2019-05-03 11:16:51 +0200

    feat: Added link buttons to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,24 @@ import React from 'react';
 import Button from "@material-ui/core/Button";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { IconButton } from '@material-ui/core';
 import Logo from './logo';
 import theme from '../theme';
 
+const container = {
+	width: `calc(100% - 200px)`,
+	marginLeft: '200px',
+	position: 'absolute',
+}
+
 const logo = {
-	flexGrow: 1,
-	marginLeft: '20px'
+	display: 'flex',
+	justifyContent: 'center',
+	margin: '1em 0'
 };
+
+const tagButton = {
+	margin: '0 1em'
+}
 
 const tagButtonText = {
 	textDecoration: 'none',
@@ -19,24 +29,31 @@ const tagButtonText = {
 
 const appBar = {
 	background: theme.palette.primary.light,
-	borderBottom: `1px solid ${theme.palette.primary.main}`,
-	boxShadow: 'none'
+	boxShadow: 'none',
 }
 
 const Header = () => (
-	<AppBar position="static" style={appBar}>
-		<Toolbar>
-			<IconButton color="inherit" aria-label="Menu">
-				<span className="material-icons">menu</span>
-			</IconButton>
-			<div style={logo}>
-				<Logo />
-			</div>
-			<Button variant="text" color="secondary">
-				<Link to="/tags" style={tagButtonText}>Tags</Link>
-			</Button>
-		</Toolbar>
-	</AppBar>
+	<div style={container}>
+		<div style={logo}>
+			<Logo />
+		</div>
+		<AppBar position="static" style={appBar}>
+			<Toolbar style={{ display: 'flex', justifyContent: 'center' }}>
+				<Button variant="text" color="secondary" style={tagButton}>
+					<Link to="/tags" style={tagButtonText}>Tags</Link>
+				</Button>
+				<Button variant="text" color="secondary" style={tagButton}>
+					<Link to="/" style={tagButtonText}>Projects</Link>
+				</Button>
+				<Button variant="text" color="secondary" style={tagButton}>
+					<Link to="/" style={tagButtonText}>Blog</Link>
+				</Button>
+				<Button variant="text" color="secondary" style={tagButton}>
+					<Link to="/" style={tagButtonText}>About</Link>
+				</Button>
+			</Toolbar>
+		</AppBar>
+	</div>
 )
 
 export default Header
