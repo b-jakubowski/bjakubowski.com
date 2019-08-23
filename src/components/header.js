63d0c24d8e28e868f6a9bb98ade03ab1f82924e3
Bartosz Jakubowski <bartosz.jakubowski@tieto.com>
2019-08-23 15:38:22 +0200

    feat: Improved header and drawer components

--- src/components/header.js
+++ src/components/header.js
@@ -1,59 +1,158 @@
-import { Link } from 'gatsby';
-import React from 'react';
-import Button from "@material-ui/core/Button";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Logo from './logo';
-import theme from '../theme';
+import React from 'react'
 
-const container = {
-	width: `calc(100% - 200px)`,
-	marginLeft: '200px',
-	position: 'absolute',
-}
+import { Link } from 'gatsby'
 
-const logo = {
-	display: 'flex',
-	justifyContent: 'center',
-	margin: '1em 0'
-};
+import clsx from 'clsx'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import Button from "@material-ui/core/Button"
+import Logo from "./logo"
 
-const tagButton = {
-	margin: '0 1em'
-}
+import DrawerTagsList from './drawer-tags-list'
+import { Typography } from "@material-ui/core"
 
-const tagButtonText = {
-	textDecoration: 'none',
-	color: theme.palette.primary.dark
-};
+const drawerWidth = 240
 
-const appBar = {
-	background: theme.palette.primary.light,
-	boxShadow: 'none',
-}
+const useStyles = makeStyles(theme => ({
+	root: {
+		display: 'flex',
+	},
+	appBar: {
+		background: 'white',
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	hide: {
+		display: 'none',
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+	},
+	drawerPaper: {
+		width: drawerWidth,
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: '0 8px',
+		...theme.mixins.toolbar,
+		justifyContent: 'space-between',
+	},
+	drawerHeaderText: {
+		marginLeft: '0.5em',
+		textTransform: 'uppercase',
+		fontWeight: 500
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		marginLeft: -drawerWidth,
+	},
+	contentShift: {
+		marginLeft: 0,
+	},
+	linkBtn: {
+		margin: '0 1em'
+	},
+	linkBtnText: {
+		textDecoration: 'none',
+		color: 'grey'
+	},
+	logo: {
+		display: 'flex',
+		justifyContent: 'center',
+		margin: '0 1em'
+	},
+}))
 
-const Header = () => (
-	<div style={container}>
-		<div style={logo}>
-			<Logo />
+const Header = () => {
+	const classes = useStyles()
+
+	const theme = useTheme()
+	const [open, setOpen] = React.useState(false)
+
+	function handleDrawerOpen() {
+		setOpen(true)
+	}
+
+	function handleDrawerClose() {
+		setOpen(false)
+	}
+
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<AppBar
+				position="fixed"
+				elevation={0}
+				className={clsx(classes.appBar, {
+					[classes.appBarShift]: open,
+				})}
+			>
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="Open drawer"
+						onClick={handleDrawerOpen}
+						edge="start"
+						className={clsx(classes.menuButton, open && classes.hide)}
+					>
+						<MenuIcon />
+					</IconButton>
+					<div className={clsx(classes.logo)}>
+						<Logo />
+					</div>
+					<Button variant="text" color="secondary" className={classes.linkBtn}>
+						<Link to="/tags" className={classes.linkBtnText}>Tags</Link>
+					</Button>
+					<Button variant="text" color="secondary" className={classes.linkBtn}>
+						<Link to="/" className={classes.linkBtnText}>Projects</Link>
+					</Button>
+					<Button variant="text" color="secondary" className={classes.linkBtn}>
+						<Link to="/" className={classes.linkBtnText}>Blog</Link>
+					</Button>
+					<Button variant="text" color="secondary" className={classes.linkBtn}>
+						<Link to="/" className={classes.linkBtnText}>About</Link>
+					</Button>
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				className={classes.drawer}
+				variant="persistent"
+				anchor="left"
+				open={open}
+				classes={{
+					paper: classes.drawerPaper,
+				}}
+			>
+				<div className={classes.drawerHeader}>
+					<Typography className={classes.drawerHeaderText}>Tags</Typography>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'ltr' ? (
+							<ChevronLeftIcon />
+						) : (
+							<ChevronRightIcon />
+						)}
+					</IconButton>
+				</div>
+				<Divider />
+				<DrawerTagsList />
+			</Drawer>
 		</div>
-		<AppBar position="static" style={appBar}>
-			<Toolbar style={{ display: 'flex', justifyContent: 'center' }}>
-				<Button variant="text" color="secondary" style={tagButton}>
-					<Link to="/tags" style={tagButtonText}>Tags</Link>
-				</Button>
-				<Button variant="text" color="secondary" style={tagButton}>
-					<Link to="/" style={tagButtonText}>Projects</Link>
-				</Button>
-				<Button variant="text" color="secondary" style={tagButton}>
-					<Link to="/" style={tagButtonText}>Blog</Link>
-				</Button>
-				<Button variant="text" color="secondary" style={tagButton}>
-					<Link to="/" style={tagButtonText}>About</Link>
-				</Button>
-			</Toolbar>
-		</AppBar>
-	</div>
-)
+	)
+}
 
 export default Header
