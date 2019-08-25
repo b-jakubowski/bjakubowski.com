03946a5c44865c58dfb9ceb5010867e91118aac4
Bartosz Jakubowski <bjakubov@gmail.com>
2019-08-25 16:15:41 +0200

    feat: Added about page

---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Button, MuiThemeProvider } from "@material-ui/core";
+import theme from "../theme";
+
+const buttonText = {
+	textDecoration: 'none',
+	color: 'white'
+}
+
+const AboutPage = () => (
+	<MuiThemeProvider theme={theme}>
+		<Layout>
+			<SEO title="Page two" />
+			<h1>Hi from the about page</h1>
+				<Link to="/" style={buttonText}>
+					<Button color="secondary" variant="contained">
+						<span className="material-icons">keyboard_arrow_left</span>
+						Home page
+					</Button>
+				</Link>
+		</Layout>
+	</MuiThemeProvider>
+)
+
+export default AboutPage
