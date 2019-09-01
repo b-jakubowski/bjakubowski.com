790f693d45a6eba5332e109c7d18c1b0194b438b
Bartosz Jakubowski <bjakubov@gmail.com>
2019-09-01 16:47:50 +0200

    feat: Added Helmet Header to layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
 
 import Header from "./header"
 import "./layout.css"
+import useSiteMetadata from "../hooks/siteMetadata"
 
-const container = {
-}
-
-const content = {
-	display: 'flex',
-	justifyContent: 'center',
-	flexDirection: 'column',
-	alignItems: 'center',
-	width: '100%',
-	margin: '10em 0 3em 0'
+const styles = {
+	container: {},
+	content: {
+		display: 'flex',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		alignItems: 'center',
+		width: '100%',
+		margin: '10em 0 3em 0'
+	}
 }
 
 const Layout = ({ children }) => {
+	const {title, description} = useSiteMetadata();
 
 	return (
-		<div style={container}>
+		<>
+			<Helmet>
+				<html lang="en" />
+				<title>{title}</title>
+				<meta name="description" content={description} />
+			</Helmet>
 			<Header />
-			<div style={content}>
+			<div style={styles.content}>
 				<main style={{ maxWidth: '960px' }}>{children}</main>
 			</div>
-		</div>
+		</>
 	)
 }
 
