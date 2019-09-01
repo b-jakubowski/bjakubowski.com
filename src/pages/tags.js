4e6a58c0aaf9412aeb44ccf1aa598151d41489a7
Bartosz Jakubowski <bjakubov@gmail.com>
2019-09-01 17:57:32 +0200

    refactor: Moved tagsList query to hook file

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
 import { MuiThemeProvider } from "@material-ui/core";
 
 import Layout from '../components/layout';
 import theme from '../theme';
+import useTagsList from '../hooks/useTagsList';
 
-function TagsPage(props) {
-	const data = props.data.allMarkdownRemark.group;
+function TagsPage() {
+	const tagsList = useTagsList();
 	return (
 		<MuiThemeProvider theme={theme}>
 			<Layout>
 				<div className="tags">
 					<h1>All tags</h1>
 					{
-						data.map((tag, index) => (
+						tagsList.map((tag, index) => (
 							<Link to={`/${tag.fieldValue}`} key={index}>
 								{tag.fieldValue} {`(${tag.totalCount})`}
 							</Link>
@@ -26,15 +27,3 @@ function TagsPage(props) {
 }
 
 export default TagsPage;
-
-export const pageQuery = graphql`
-	query {
-		allMarkdownRemark(
-			limit: 2000) {
-			group(field: frontmatter___tags) {
-				fieldValue
-				totalCount
-			}
-		}
-	}
-`
