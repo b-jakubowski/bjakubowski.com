d6d448c36a13d004c3afb7016e188f67ec5ce2f8
Bartosz Jakubowski <bjakubov@gmail.com>
2019-09-01 17:07:22 +0200

    refactor: Abstract blogPostList query to hook file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { MuiThemeProvider } from "@material-ui/core";
 
 import Layout from '../components/layout';
-import { MuiThemeProvider } from "@material-ui/core";
 import theme from '../theme';
 import BlogPostsList from "../components/blog-posts-list";
+import useBlogPostList from "../hooks/useBlogPostList";
 
-const IndexPage = (props) => {
-	const postList = props.data.allMarkdownRemark;
+const IndexPage = () => {
+	const postList = useBlogPostList();
 
 	return (
 		<MuiThemeProvider theme={theme}>
@@ -21,34 +21,3 @@ const IndexPage = (props) => {
 }
 
 export default IndexPage;
-
-
-export const listQuery = graphql`
-	query ListQuery {
-		allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }){
-			edges{
-				node{
-					frontmatter{
-						title
-						date(formatString: "MMMM Do YYYY")
-						tags
-						image {
-							childImageSharp {
-								resize(width: 1500, height: 1500) {
-									src
-								}
-								fluid(maxWidth: 786) {
-									...GatsbyImageSharpFluid
-								}
-							}
-						}
-					}
-					excerpt(pruneLength: 250)
-					fields{
-						slug
-					}
-				}
-			}
-		}
-	}
-`
