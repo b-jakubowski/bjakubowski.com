b155f7245395f3aeb615065f9a4fa24226eaf04e
Bartosz Jakubowski <bjakubov@gmail.com>
2019-09-02 19:47:17 +0200

    feat: Optimize image in tools section

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import { Button, MuiThemeProvider } from "@material-ui/core";
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import theme from "../theme"
-import toolsImg from '../../images/tools.jpg'
 import ToolsList from "../components/tools-list"
 import toolsListJson from "../assets/tools"
 
@@ -25,26 +25,40 @@ const styles = {
 	}
 }
 
-const ToolsPage = () => (
-	<MuiThemeProvider theme={theme}>
-		<Layout>
-			<article>
-				<header>
-					<figure>
-						<img src={toolsImg} alt="tools img" width="840" height="552" style={styles.headerImg} />
-					</figure>
-					<h1>Useful tools for frontend developers</h1>
-				</header>
-				<ToolsList props={toolsListJson} />
-			</article>
-			<Link to="/">
-				<Button color="secondary" variant="contained" style={styles.buttonText}>
-					<span className="material-icons">keyboard_arrow_left</span>
-					Home page
-				</Button>
-			</Link>
-		</Layout>
-	</MuiThemeProvider>
-)
+const ToolsPage = () => {
+	const { image } = useStaticQuery(graphql`
+		query {
+			image: file(relativePath: { eq: "tools.jpg"}) {
+				sharp: childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid_withWebp
+					}
+				}
+			}
+		}
+	`);
+
+	return(
+		<MuiThemeProvider theme={theme}>
+			<Layout>
+				<article>
+					<header>
+						<figure>
+							<Img fluid={image.sharp.fluid} style={styles.headerImg} />
+						</figure>
+						<h1>Useful tools for frontend developers</h1>
+					</header>
+					<ToolsList props={toolsListJson} />
+				</article>
+				<Link to="/">
+					<Button color="secondary" variant="contained" style={styles.buttonText}>
+						<span className="material-icons">keyboard_arrow_left</span>
+						Home page
+					</Button>
+				</Link>
+			</Layout>
+		</MuiThemeProvider>
+	)
+}
 
 export default ToolsPage
